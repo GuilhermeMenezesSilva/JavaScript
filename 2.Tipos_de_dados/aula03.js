fcda7e9eb29fa8f42e5472990c3501a19b954775
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-04-03 15:12:04 -0300

    Number + validação do tipo

---
+++ 2.Tipos_de_dados/aula03.js
@@ -0,0 +1,11 @@
+//Exibir valores no console
+console.log(5)
+console.log(12.5)
+console.log(-123)
+
+console.log()
+
+// Verificar o tipo de dado
+console.log(typeof (5))
+console.log(typeof (12.5))
+console.log(typeof (-123))
