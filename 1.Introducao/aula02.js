ebaeba669dfc590088241d3457be9b65fb6c4199
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-04-03 14:49:52 -0300

    Tipos de comentários

---
+++ 1.Introducao/aula02.js
@@ -0,0 +1,13 @@
+console.log("Comentários não são exibidos ao rodar o programa")
+
+// não será exibido
+console.log("O comentário acima é do tipo linha, não será mostrado")
+
+console.log("Existem os comentárioos do tipo bloco.")
+/*
+    bom dia
+    boa tarde
+    boa noite 
+    boa madrugada
+*/
+console.log("As palavras comentadas acima não serão exibidas")
