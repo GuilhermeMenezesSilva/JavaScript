364759bd95f8f003bf8f77d3e331620df7a461e7
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-04-03 23:25:56 -0300

    Variavel e suas formas de declaração

---
+++ 3.LógicaDeProgramacao/aula07.js
@@ -0,0 +1,6 @@
+// Declarando uma variavel
+let nomeCompleto = 'Guilherme Menezes da Silva'
+const anoDeNascimento = 2002
+
+// Utilizando a variavel
+console.log(`${nomeCompleto} nasceu em ${anoDeNascimento}`)
