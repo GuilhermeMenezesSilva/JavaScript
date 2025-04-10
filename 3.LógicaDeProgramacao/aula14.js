54bf22c73833fa2d8373300146211e601d862ba6
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-04-10 12:06:15 -0300

    Saída de um loop: break

---
+++ 3.LógicaDeProgramacao/aula14.js
@@ -0,0 +1,17 @@
+// break
+
+let nome = "Guilherme"
+
+for (let i = 0; i < 10; i + 1) {
+
+    if (i === 3) {
+        nome = "Matheus"
+    }
+
+    if (i === 5 && nome === "Matheus") {
+        console.log("O nome é Matheus, pode parar")
+        break
+    }
+
+    console.log(i)
+}
