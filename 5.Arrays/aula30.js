0624ba18e17444410f3d9307dc0a73a6cb4e3d6a
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-07 17:18:25 -0300

    Percorrendo um array com loop

---
+++ 5.Arrays/aula30.js
@@ -0,0 +1,12 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i])
+}
+
+console.log('------------------')
+
+numeros.forEach(element => {
+    console.log(element);
+    
+});
