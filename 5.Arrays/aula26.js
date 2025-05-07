e7bdce8c5f98692b32347f28e5a022a91df87cda
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-07 16:31:51 -0300

    definicição de array

---
+++ 5.Arrays/aula26.js
@@ -0,0 +1,11 @@
+// Definindo array e seus tipos
+
+let arr = [1, 2, 3, 4, 5]
+let nomes = ["Matheus", "Vinicius", "Guilherme"]
+let bool = [true, false, true, false]
+let misturado = ["Guilherme", 22, true]
+
+console.log(arr)
+console.log(nomes)
+console.log(bool)
+console.log(misturado)
