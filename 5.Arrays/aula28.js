d237729e441b6a64518680909c1772675f59967a
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-07 17:02:14 -0300

    Adicionando elemento no array

---
+++ 5.Arrays/aula28.js
@@ -0,0 +1,15 @@
+// push (adiciona no fim) e unshift (adiciona no inicio) 
+let pessoas = ["Rose", "Sergio", "Vinicius", "Guilherme", "Matheus"]
+console.log("Lista da familia: ", pessoas)
+
+console.log()
+
+pessoas.push("Elaine")
+console.log("Lista da familia depois de 2019: ", pessoas)
+
+console.log()
+
+pessoas.unshift('Spyke');
+console.log("Lista da familia depois de 2022: ", pessoas)
+
+
