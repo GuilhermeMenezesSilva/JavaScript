a50a7ad1da2f6a40d58e3a276bebaff7b8b28550
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-20 15:55:30 -0300

    renomeado o arquivo

--- 5.Arrays/aula38.js
+++
@@ -1,8 +0,0 @@
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-const total = numeros.reduce(function (acumulador, valor) {
-    console.log(acumulador, valor)
-    acumulador += valor
-    return acumulador
-
-}, 0 /*<- valor definido para que o acumulador inicie por ele*/)
-console.log(total)
