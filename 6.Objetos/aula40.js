ea720ec24806fe1bb8f9760143a72480557274de
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-20 15:46:39 -0300

    Refactor: remoção de comentarios

--- 6.Objetos/aula40.js
+++ 6.Objetos/aula40.js
@@ -7,12 +7,10 @@ console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.idade} anos e sou ${pess
 
 console.log("----------")
 
-// Adicionando propriedade
 pessoa.casado = false
 console.log(`A noticia de que sou casado é ${pessoa.casado}.`)
 
 console.log("----------")
 
-// Apagando propriedade
 delete pessoa.nome
 console.log(pessoa)
