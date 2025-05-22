cbbeedfa70b6a8cec51b001f0884fb123827c9fe
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-22 17:14:11 -0300

    // Factory Functions

--- 4.Funcao/aula23.js
+++ 4.Funcao/aula23.js
@@ -1,23 +1,28 @@
-// IIFE 
-// Toca o escopo global:
-function qualquerCoisa() {
-    console.log(1223432)
+// Factory Functions
+
+function criaPessoa(nome, sobrenome, altura, peso) {
+    return {
+        nome,
+        sobrenome,
+        // Quando se tem uma função dentro de outra função ela se chama MÉTODO
+        fala(assunto) {
+            return `${this /*Se refere ao objeto que o chama*/.nome} está ${assunto}.`
+        },
+        altura,
+        peso,
+        imc() {
+            const indice = this.peso / (this.altura ** 2)
+            return `Seu IMC é ${indice.toFixed(2)}`
+        }
+    }
 }
-qualquerCoisa()
 
-    // Não toca:
-    (function (idade, peso, altura) {
+const p1 = criaPessoa('Guilherme', 'Menezes', 1.82, 82)
+console.log(p1.fala('falando sobre JS'))
+console.log(p1.imc())
 
-        const sobrenome = 'Menezes'
-        function criaNome(nome) {
-            return nome + ' ' + sobrenome
-        }
+console.log()
 
-        function falaNone() {
-            console.log(criaNome('Guilherme'))
-        }
-        
-        falaNone()
-        console.log(idade, peso, altura)
-
-    })(20, 82, 1.82);
+const p2 = criaPessoa('Vinicius', 'Menezes', 1.90, 84)
+console.log(p2.fala('falando sobre Java'))
+console.log(p2.imc())
