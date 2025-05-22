efca2956ce21fb2a0ad3025105aa57a2bba8ed87
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-22 16:38:05 -0300

    Estrutura de controle com operador ternário

---
+++ 3.Logica_de_Programacao/aula10.js
@@ -0,0 +1,5 @@
+// (condição) ? 'Valor verdadeiro' : 'Valor falso'
+
+const pontuacaoUsuario = 1000 
+const nivelUsuario = pontuacaoUsuario >= 100 ? 'Usuario vip' : 'Usuario normal'
+console.log(nivelUsuario)
