449640d821e4de9529c49e12ff225144cbf13ef7
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-22 16:43:18 -0300

    Pular execução de loop com continue

---
+++ 3.Logica_de_Programacao/aula14.js
@@ -0,0 +1,10 @@
+// continue
+
+for (let i = 10; i > 0; i = i - 1) {
+    if (i === 2) {
+        console.log('Pulei o número 2')
+        continue // Pula para próxima interação
+    }
+
+    console.log(i)
+}
