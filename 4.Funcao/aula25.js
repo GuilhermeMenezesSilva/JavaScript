a475e64d84a8048196c13e552f4f25ceca624280
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-22 17:14:29 -0300

    Função recursiva

--- 4.Funcao/aula25.js
+++ 4.Funcao/aula25.js
@@ -1,20 +1,8 @@
-// Constructor Functions
-
-function Pessoa(nome, sobrenome) {
-    // Atributos ou métodos privados, ou seja, não podem ser acessados com a notação de ponto
-    const Id = 123456
-    const metodoInterno = function () {
-    }
-
-    // Atributos ou métodos publicos, ou seja, podem ser acessados com a notação de ponto
-    this.nome = nome
-    this.sobrenome = sobrenome
-
-    this.metodo = function () {
-        console.log(this.nome + ': sou um método')
-    };
+function recursiva(max) {
+    if (max >= 10) return;
+    max++;
+    console.log(max);
+    recursiva(max);
 }
-        // new instancia o objeto anteriormente criado  
-const p1 = new Pessoa('Luiz', 'Miranda')
-const p2 = new Pessoa('Guilherme', 'Menezes')
-p1.metodo()
+
+recursiva(-10);
