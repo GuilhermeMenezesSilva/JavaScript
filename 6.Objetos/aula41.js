af9be1a83620665df5d930a89d6b544632b3ce0a
GuilhermeMenezesSilva <guissilvamenezes@gmail.com>
2025-05-22 09:40:58 -0300

    Destructuring com objetos

---
+++ 6.Objetos/aula41.js
@@ -0,0 +1,13 @@
+const carro = {
+    rodas: 4,
+    porta: 4,
+    tetoslar: true,
+    motor: 1.8
+}
+
+const {rodas: vroda, porta: vporta, tetoslar: vtetosolar, motor: vmotor} = carro
+
+console.log(vporta)
+console.log(vroda)
+console.log(vtetosolar)
+console.log(vmotor)
